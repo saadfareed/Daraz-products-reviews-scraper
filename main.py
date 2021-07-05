1977a7e1e8f58656106a669d9b1569ba0f679a78
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2021-07-05 19:31:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,28 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
-driver.get("https://www.daraz.com.bd/bath-body/?page=2&spm=a2a0e.home.cate_4.1.25e84591fd3uEZ")
-
+##driver.get("https://www.daraz.com.bd/bath-body/?page=2&spm=a2a0e.home.cate_4.1.25e84591fd3uEZ")
+list_link=[
+    "https://www.daraz.com.bd/cereal/?page=1&spm=a2a0e.home.cate_6_2.2.107a12f7jJBzfg",
+    "https://www.daraz.com.bd/cereal/?page=2&spm=a2a0e.home.cate_6_2.2.107a12f7jJBzfg",
+    "https://www.daraz.com.bd/cereal/?page=3&spm=a2a0e.home.cate_6_2.2.107a12f7jJBzfg",
+    "https://www.daraz.com.bd/cereal/?page=4&spm=a2a0e.home.cate_6_2.2.107a12f7jJBzfg",
+    "https://www.daraz.com.bd/cereal/?page=5&spm=a2a0e.home.cate_6_2.2.107a12f7jJBzfg",
+    "https://www.daraz.com.bd/cereal/?page=6&spm=a2a0e.home.cate_6_2.2.107a12f7jJBzfg",
+    "https://www.daraz.com.bd/cereal/?page=7&spm=a2a0e.home.cate_6_2.2.107a12f7jJBzfg",
+    "https://www.daraz.com.bd/mother-baby/?spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=2&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=3&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=4&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=5&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=6&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=7&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=8&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=9&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=10&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=11&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k",
+    "https://www.daraz.com.bd/mother-baby/?page=12&spm=a2a0e.searchlistcategory.cate_5.1.6d4158dfpM1n6k"
+]
 i=1
 comments=[]
 label=[]
@@ -41,17 +61,21 @@ def change_page(driver,p):
             v = v + 1
         except Exception as e:
             break
+for i in range(len(list_link)):
+    driver.get(list_link[i])
+    time.sleep(10)
+    i=1
+    while 1:
+        try:
+            name = driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div[1]/div/div[1]/div[2]/div'+str([i])+'/div/div/div[1]/div')
+            herf=name.find_element_by_css_selector('a').get_attribute('href')
 
-while 1:
-    try:
-        name = driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div[1]/div/div[1]/div[2]/div'+str([i])+'/div/div/div[1]/div')
-        herf=name.find_element_by_css_selector('a').get_attribute('href')
-        print(herf)
-        i += 1
-        link.append(herf)
-    except Exception as e:
-        print('finished')
-        break
+            print(herf)
+            i += 1
+            link.append(herf)
+        except Exception as e:
+            print('finished')
+            break
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 for i in range(len(link)-1):
     driver.get(link[i])
@@ -74,4 +98,8 @@ for i in range(len(link)-1):
         None
 df = pd.DataFrame({"text":comments,
                          "label":label})
-df.to_csv('data.csv', encoding='utf-8')
+df.drop_duplicates(subset ="text",
+                     keep = False, inplace = True)
+# drop all rows with any NaN and NaT values
+df= df.dropna()
+df.to_csv('data1.csv', encoding='utf-8')
